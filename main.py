c1d8d450225ece0025da2cb1894cbe39496d6c70
mtsadler <michaeltsadler1@gmail.com>
2023-01-05 18:37:27 -0600

    loop over some sample precisions

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 def calculate_pi(accuracy = 100):
-    print(f"Using the Gregory Leibniz Theory ({ accuracy } iterations)")
-    result = 0
+    estimate = 0
     for index in range(accuracy):
         sign = -1 if index % 2 == 1 else 1
         denominator = index * 2 + 1
-        result += sign * (4 / denominator)
-        # print(f"#{ index }: { result }")
-    return result
+        estimate += sign * (4 / denominator)
+    return estimate
 
 
 if __name__ == "__main__":
-    estimate = calculate_pi(accuracy=1000000)
-    print(f"Estimate: { estimate }")
+    print(f"Calculating Pi using Gregory Leibniz's Series.")
+    precisions = [1,2,3,4,5,10,100,1000,10000,100000,1000000,5000000]
+    for precision in precisions:
+        estimate = calculate_pi(accuracy=precision)
+        print(f"{ round(estimate, 10) } ({ precision } iterations)")
